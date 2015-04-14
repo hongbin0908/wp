25611dca63c4ac3835b6a76564af4d1c5f867dae
Bin Hong <hongbin0908@126.com>
2015-04-15 00:22:18 +0800

    spark/

---
+++ spark/SparkTests/src/WordCount.scala
@@ -0,0 +1,17 @@
+
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object WordCount {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("WordCount")
+    conf.setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val textRDD = sc.textFile(args(1))
+    val result = textRDD.flatMap {
+      line => line.split(" ")
+    }.map(word => (word,1)).reduceByKey(_ + _)
+  }
+}
