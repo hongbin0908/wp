a24f2ad0806a20121b23d52d54fe5798be9e6156
Bin Hong <hongbin0908@126.com>
2015-04-15 11:25:28 +0800

    add SparkTest

--- spark/SparkTests/src/WordCount.scala
+++ spark/SparkTests/src/WordCount.scala
@@ -9,9 +9,11 @@ object WordCount {
     conf.setMaster("local")
     val sc = new SparkContext(conf)
 
-    val textRDD = sc.textFile(args(1))
+    val textRDD = sc.textFile("wp/spark/WordCount/word_count_in.3")
     val result = textRDD.flatMap {
       line => line.split(" ")
     }.map(word => (word,1)).reduceByKey(_ + _)
+    
+    result.saveAsSequenceFile("wp/spark/WordCount/word_count_out.3")
   }
 }
